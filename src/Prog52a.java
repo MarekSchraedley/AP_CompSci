bc7c4dd80ba43240870da041bc4e05d9ee7ffbf0
Marek Schraedley <schraedl.m@stu.janesville.k12.wi.us>
2023-09-05 10:42:45 -0500

    Initial commit

---
+++ src/Prog52a.java
@@ -0,0 +1,16 @@
+public class Prog52a {
+    public static void main(String[] args) {
+        int length = 143;
+        int width = 82;
+
+        int area = length * width;
+        int perim = 2*length + 2*width;
+
+        System.out.println("area = " + area);
+        System.out.println("perim = " + perim);
+    }
+}
+/*
+area = 11726
+perim = 450
+ */
