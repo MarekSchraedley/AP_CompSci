4f878ec1337d644b1066a6dbfcb25edfc9174313
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2023-09-07 11:24:16 -0500

    Update

--- src/Prog54c.java
+++ src/Prog54c.java
@@ -1,10 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Prog54c {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter radius: ");
-        double radius = input.nextInt();
-        double pi = 3.14159
+        double radius = input.nextDouble();
+        double pi = 3.14159;
+        double area = Math.pow(radius, 2) * pi;
+        double circum = pi * radius * 2;
+        System.out.printf("Radius: %.3f", radius);
+        System.out.println();
+        System.out.printf("Area: %.3f", area);
+        System.out.println();
+        System.out.printf("Circumference: %.3f", circum);
+
     }
 }
---
+++ src/Prog58b.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Prog58b {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter A: ");
+        String A = keyboard.nextLine();
+
+        System.out.print("Enter B: ");
+        Double B = keyboard.nextDouble();
+
+        System.out.print("Enter C: ");
+        Double C = keyboard.nextDouble();
+
+    }
+}
