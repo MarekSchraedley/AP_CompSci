c2601f73bdfafd6ebe8662458d21cb7a6f4d2512
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2023-09-25 11:25:02 -0500

    Update

---
+++ src/LP4_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LP4_1 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the number of copies to be printed");
+        int copies = keyboard.nextInt();
+        int rate = 0; //Finish this up
+    }
+}
