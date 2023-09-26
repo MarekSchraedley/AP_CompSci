61ff4d0808df5a83a936081944b79574c9f96f6b
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2023-09-26 11:25:54 -0500

    Update

--- src/LP4_1.java
+++ src/LP4_1.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class LP4_1 {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter the number of copies to be printed");
+        System.out.print("Enter the number of copies to be printed: ");
         int copies = keyboard.nextInt();
-        int rate = 0; //Finish this up
+        double rate = 0; //Finish this up
+        if (0 < copies && copies < 99) {
+            rate = 0.30;
+        }else if (100 <= copies && copies <= 499) {
+            rate = 0.28;
+        }else if (500 <= copies && copies <= 749) {
+            rate = 0.27;
+        }else if (750 <= copies && copies <= 1000) {
+            rate = 0.26;
+        }else if (1000 < copies) {
+            rate = 0.25;
+        }
+        System.out.println("Price per copy is: $" + rate);
+        System.out.printf("Total cost is: $%.2f", (copies * rate));
     }
 }
---
+++ src/LP4_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LP4_2 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter package weight in kilograms: ");
+        int kilo = keyboard.nextInt();
+        System.out.print("Enter package length in centimeters: ");
+        int len = keyboard.nextInt();
+        System.out.print("Enter package width in centimeters: ");
+        int wid = keyboard.nextInt();
+        System.out.print("Enter package height in centimeters: ");
+        int height = keyboard.nextInt();
+        if (len*wid*height > 10000) {
+            if (kilo > 27) {
+                System.out.print("Too heavy and too large");
+            }else{
+                System.out.print("Too large");
+            }
+        }else{
+            System.out.print("Too heavy"); //Finish this up. Make an acceptable package possibility.
+        }
+    }
+}
