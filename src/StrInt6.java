44caafb10b9695643b0273f9a1305c5faf8db0ff
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2023-09-28 12:39:13 -0500

    Update

--- src/StrInt6.java
+++ src/StrInt6.java
@@ -8,15 +8,25 @@ public class StrInt6 {
         String mytext = cool.nextLine().toLowerCase().trim();
         int count = 0;
         int count2 =0;
+        int check = 0;
+        int letters = 0;
         String str = "";
-        while (count != mytext.length());
+        while (count != mytext.length()) {
+            check = 0;
+            count = 0;
             if (str.length() == 0) {
                 str += mytext.substring(count, count+1);
-            }else{
-            while (count2 != str.length()); //iterate through every thing in str before confirming
-
-                count2++;
+            }else {
+                while (count2 != str.length()) {
+                    if (!mytext.substring(count, count + 1).equals(str.substring(count, count + 1)))
+                        check += 1;
+                    count2++;
+                }
+                if (check == count2)
+                    letters += 1;//finish this up
+            }
             count++;
         }
+        System.out.print("letters:" + letters);
     }
 }
