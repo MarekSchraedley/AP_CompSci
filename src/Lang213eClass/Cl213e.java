26fc56bbd3c699ec0e2a0e8706bdd5989180131f
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2023-10-24 11:26:56 -0500

    Update

--- src/Lang213eClass/Cl213e.java
+++ src/Lang213eClass/Cl213e.java
@@ -1,26 +1,45 @@
 package Lang213eClass;
 
 public class Cl213e {
-    private int myAge;
-    private int from0_19;
-    private int from20_39;
-    private int from40_59;
-    private int from79_;
+    private double myAge;
+    private double from0_19;
+    private double from20_39;
+    private double from40_59;
+    private double from60_79;
+    private double from79_;
+    private double sum;
 
 
-    public Cl213e(int age) {
+
+
+    public void calc(int age)
+    {
         myAge = age;
-        from0_19 = 0;
-        from20_39 = 0;
-        from40_59 = 0;
-        from79_ = 0;
+        if (myAge<20) {
+            from0_19++;
+        }
+        else if (myAge>=20 && myAge<=39) {
+            from20_39++;
+        }
+        else if (myAge>=40 && myAge<=59) {
+            from40_59++;
+        }
+        else if (myAge>=60 && myAge<=79) {
+            from60_79++;
+        }
+        else if (myAge>79) {
+            from79_++;
+        }
     }
 
-    public void calc() {
-        if (myAge<20)
-    }
-
-    public void ToString() {
+    public String toString() {
         //return everything in one big chunk & do percentages on the spot
+        sum = from0_19+from20_39+from40_59+from60_79+from79_;
+        return "Age group\tDistribution\tPercentage\n" +
+                "<20\t\t\t" + from0_19 + "\t\t\t\t" + Math.round(from0_19/sum*100.0) + "\n" +
+                "20-39\t\t\t" + from20_39 + "\t\t\t\t" + Math.round(from20_39/sum*100.0) + "\n" +
+                "40-59\t\t\t" + from40_59 + "\t\t\t\t" + Math.round(from40_59/sum*100.0) + "\n" +
+                "60-79\t\t\t" + from60_79 + "\t\t\t\t" + Math.round(from60_79/sum*100.0) + "\n" +
+                ">79\t\t\t" + from79_ + "\t\t\t\t" + Math.round(from79_/sum*100.0);
     }
 }
--- src/Lang213eClass/Prog213eClass.java
+++ src/Lang213eClass/Prog213eClass.java
@@ -1,5 +1,7 @@
 package Lang213eClass;
 
+import Prog213b.Cl213b;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,10 +10,13 @@ public class Prog213eClass {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("Langdat/prog213e.dat"));
-
+            int age = 0;
+            Cl213e wowsa = new Cl213e();
             while (input.hasNext()) {
-                int age = input.nextInt();
+                age = input.nextInt();
+                wowsa.calc(age);
             }
+            System.out.println(wowsa.toString());
 
         } catch (IOException e) {
             System.out.println("cant find data file!");
