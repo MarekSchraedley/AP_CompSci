3b6d015dba2fd7e2586a2ff83f90884b6b756299
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2023-11-28 21:06:59 -0600

    Add files via upload

---
+++ src/Prog214bcl/prog214b.java
@@ -0,0 +1,25 @@
+package Prog214bcl;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class prog214b {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog214b.dat"));
+            while (input.hasNext()) {
+                double ID = input.nextDouble();
+                double ytd = input.nextDouble();
+                double bp = input.nextDouble();
+                double decshift = input.nextDouble();
+                double hours = input.nextDouble();
+                Cl214b wowsa = new Cl214b(ID, ytd, bp, decshift, hours);
+                wowsa.calc();
+                System.out.println(wowsa.toString());
+            }
+
+        } catch (IOException e) {
+            System.out.println("cant find data file!");
+        }
+    }
+}
