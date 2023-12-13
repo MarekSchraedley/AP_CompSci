f0862b14a32ac288c44935498343950bd5f28a41
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2023-12-13 11:36:45 -0600

    Merge remote-tracking branch 'origin/master'

--- src/Q2/ArrayListLab/IntArrayList.java
+++ src/Q2/ArrayListLab/IntArrayList.java
@@ -33,8 +33,31 @@ public class IntArrayList {
     }
 
     public void add(int num) {
-        if (mySize+1==myCapacity) {}
+        if (mySize+1==myCapacity) {doubleCapacity();}
+        myArray[mySize] = num;
+        mySize++;
     }
 
+    public void remove(int index) {
+        for (int lcv = index+1; lcv < mySize; lcv++) {
+            myArray[lcv-1] = myArray[lcv];
+            mySize--;
+            if (mySize <= (myCapacity * 0.25)) {halfCapacity();}
+        }
+    }
+    public int get(int index) {return myArray[index];}
+
+    public void set(int index, int num) {myArray[index] = num;}
+
+    public int size() {return mySize;}
+
+    public int[] toArray() {return myArray;}
+
+    public int indexOf(int num) {
+        for (int lcv = 0; lcv < mySize; lcv++) {
+            if (myArray[lcv] == num) return lcv;
+        }
+        return -1;
+    }
 
 }
