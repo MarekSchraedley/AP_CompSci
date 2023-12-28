86bd04a2842b0daffc4a957b717b8a1eb4a086f9
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2023-12-28 13:37:37 -0600

    Add files via upload

---
+++ src/Q2/Prog214c/prog214c.java
@@ -0,0 +1,26 @@
+package Prog214c;
+import Prog214bcl.Cl214b;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class prog214c {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/Prog214c.dat"));
+
+            while (input.hasNext()) {
+                String type = input.next();
+                double gallons = input.nextDouble();
+                String carwash = input.next();
+                Cl214c wowsa = new Cl214c(gallons, type, carwash);
+                wowsa.calc();
+                System.out.println(wowsa.toString());
+            }
+
+        } catch (IOException e) {
+            System.out.println("cant find data file!");
+        }
+    }
+}
