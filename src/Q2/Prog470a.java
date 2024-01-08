e771cdd7c091bc9ba6255b9385792ef4286d4043
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-01-08 11:28:25 -0600

    Merge remote-tracking branch 'origin/master'

--- src/Q2/Prog470a.java
+++ src/Q2/Prog470a.java
@@ -12,17 +12,25 @@ public class Prog470a {
             int[][] list1 = new int[6][5];
             int count = 0;
             System.out.println("Original Table");
-            for(int lcv = 0; lcv < list1.length; lcv++) {
+            for(int lcv = 0; lcv < list1.length-1; lcv++) {
                 for (int lcv2 = 0; lcv2 < list1[0].length; lcv2++) {
-                    if (lcv == list1.length-1) {
-                        list1[lcv][lcv2] = count;
-                    } else {
-                        list1[lcv][lcv2] = input.nextInt();
-                    }
-                    System.out.print(list1[lcv][lcv2] + " "); //finish this up
-
+                    list1[lcv][lcv2] = input.nextInt();
+                    System.out.print(list1[lcv][lcv2] + " ");
                 }
-                count = 0;
+                System.out.println();
+            }
+            System.out.println();
+
+            for(int lcv = 0; lcv < list1.length-1; lcv++) {
+                for (int lcv2 = 0; lcv2 < list1[0].length; lcv2++) {
+                    list1[5][lcv2] += list1[lcv][lcv2];
+                }
+            }
+
+            for(int lcv = 0; lcv < list1.length; lcv++) {
+                for (int lcv2 = 0; lcv2 < list1[0].length; lcv2++) { //fix this program (rotate 90 degrees?)
+                    System.out.print(list1[lcv][lcv2] + " ");
+                }
                 System.out.println();
             }
 
