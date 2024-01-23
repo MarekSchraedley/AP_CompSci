1337affffb130472fbc628f26ae9adc51ff1593a
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-01-23 11:27:50 -0600

    Merge remote-tracking branch 'origin/master'

--- src/Q3/Prog701g/Prog701g.java
+++ src/Q3/Prog701g/Prog701g.java
@@ -1,17 +1,20 @@
-package Q3;
+package Q3.Prog701g;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class prog477a {
+public class Prog701g {
     public static void main(String[] args) {
         try {
-            Scanner input = new Scanner(new File("Langdat/prog477a.dat"));
-            int[][] array = new int[4][5];
+            Scanner input = new Scanner(new File("Langdat/prog701g.dat"));
+            List<Person> list = new ArrayList<Person>();
 
+            while (input.hasNext()) {
 
+            }
 
         } catch (IOException e) {
             System.out.println("cant find data file!");
---
+++ src/Q3/Prog701g/names.java
@@ -0,0 +1,11 @@
+package Q3.Prog701g;
+
+// An interface garuantees that anything that
+// implements it will have to impliment these
+// metheds before the code will compile
+public interface names {
+    public String getFirst();
+    public String getLast();
+    public String getName();
+
+}
