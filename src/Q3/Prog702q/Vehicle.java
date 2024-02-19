ca77e279082a56e10026ec421e8100d87f42aee6
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-02-19 11:27:04 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/Q3/Prog702q/Vehicle.java
@@ -0,0 +1,16 @@
+package Q3.Prog702q;
+
+public class Vehicle extends names {
+    String myName = "";
+    int myTires = 0;
+    double myValue = 0;
+    public Vehicle(String name, int tires, double value) {
+        myName = name;
+        myTires = tires;
+        myValue = value;
+    }
+
+    public String getName() {return myName;}
+    public int getTires() {return myTires;}
+    public double getValue() {return myValue;}
+}
