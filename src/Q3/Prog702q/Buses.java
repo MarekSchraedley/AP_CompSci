c33b79bb5695c31369f6b3988f63acec12ac7d6e
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-02-27 11:26:11 -0600

    Merge remote-tracking branch 'origin/master'

--- src/Q3/Prog702q/Buses.java
+++ src/Q3/Prog702q/Buses.java
@@ -2,17 +2,11 @@ package Q3.Prog702q;
 
 public class Buses extends Vehicle
 {
-    String myName = "";
-    int myTires = 0;
-    double myValue = 50000;
     String myHome = "";
     public Buses(String name, int tires, double value, String home) {
         super(name, tires, 50000);
         myHome = home;
     }
 
-    public String getName() {return myName;}
-    public int getTires() {return myTires;}
-    public double getValue() {return myValue;}
     public String getHome() {return myHome;}
 }
--- src/Q3/Prog702q/Cars.java
+++ src/Q3/Prog702q/Cars.java
@@ -1,14 +1,9 @@
 package Q3.Prog702q;
 
 public class Cars extends Vehicle {
-    String myName = "";
-    int myTires = 0;
     double myValue = 0;
     public Cars(String name, int tires, double value) {
         super(name, tires, value);
     }
 
-    public String getName() {return myName;}
-    public int getTires() {return myTires;}
-    public double getValue() {return myValue;}
 }
