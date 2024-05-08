d96cfa37edc7e4a3d978da853f00052799fdeece
MarekSchraedley <schraedl.m@stu.janesville.k12.wi.us>
2024-05-07 20:28:18 -0500

    Changes

--- src/Q4/LibraryManagementLab/Library.java
+++ src/Q4/LibraryManagementLab/Library.java
@@ -11,7 +11,7 @@ public class Library implements LibrarySystem {
     private ArrayList<Transaction> transactions;
 
     // TODO: Implement the constructor
-
+    public Library() {}
     // Implement interface methods
     @Override
     public void addBook(Book book) { /* Implementation; remember to add in sorted order */
@@ -88,6 +88,16 @@ public class Library implements LibrarySystem {
     public void viewMostRecentTransaction(String isbn) {
         // Hint: Use a backward loop to find the most recent transaction
         // If no transaction is found, print "No transactions found for ISBN: <isbn>"
+        boolean transactionFound = false;
+        for (int lcv = transactions.size(); lcv >= 0; lcv--) {
+            if (transactions.get(lcv).getIsbn().equals(isbn) && transactionFound == false) {
+                System.out.println("The most recent transaction was done by this patron: " + transactions.get(lcv).getPatronId());
+                transactionFound = true;
+            }
+        }
+        if (transactionFound == false) {
+            System.out.println("No transactions found for ISBN: " + isbn);
+        }
     }
 
     // TODO: Complete the implementation of LibrarySystem methods
@@ -96,11 +106,28 @@ public class Library implements LibrarySystem {
     @Override
     public Book findClosestBook(String title) {
         // TODO: Search for the closest book title using .toLowerCase() and .contains(); return the closest book or null
+        for (Book book : books) {
+            if (book.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                return book;
+            }
+            if (title.toLowerCase().contains(book.getTitle().toLowerCase())) {
+                return book;
+            }
+        }
+        return books.get(0);
     }
 
     @Override
     public Book searchBookByTitle(String title) {
         // TODO: Binary search for book; if not found, return the closest book
+        BinarySearchUtil b = new BinarySearchUtil();
+        return b.binarySearchTitle(books, title);
+    }
+
+    public Book searchBookByAuthor(String author) {
+        // TODO: Binary search for book; if not found, return the closest book
+        BinarySearchUtil b = new BinarySearchUtil();
+        return b.binarySearchTitle(books, author);
     }
 
     // You might want to add some helper methods here like getBookByIsbn, getPatronById, etc.
