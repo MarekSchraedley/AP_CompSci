af3e36f55a68fbf275b19eb67ac84556d5d7a534
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-05-28 11:26:50 -0500

    update

---
+++ src/Q4/Prog7301m/NNTest.java
@@ -0,0 +1,8 @@
+package Q4.Prog7301m;
+
+public class NNTest {
+    public static void main(String[] args) {
+        double[][] X_Train = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        double[][] y_Train = {{0}, {1}, {1}, {0}};
+        var model = njew
+}
